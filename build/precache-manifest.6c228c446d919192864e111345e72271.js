7728d1c6da3cbeeda3e427d3a62dca8f94be75f0
Pawel Plaszczyk <pawelp73@gmail.com>
2018-10-28 20:33:01 +0100

    adding production build, unignoring it :-)

---
+++ build/precache-manifest.6c228c446d919192864e111345e72271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1a6bce8b5d180e1a06de",
+    "url": "/static/js/main.1a6bce8b.chunk.js"
+  },
+  {
+    "revision": "4d1314caa17bb049632b",
+    "url": "/static/js/1.4d1314ca.chunk.js"
+  },
+  {
+    "revision": "1a6bce8b5d180e1a06de",
+    "url": "/static/css/main.57383655.chunk.css"
+  },
+  {
+    "revision": "4d1314caa17bb049632b",
+    "url": "/static/css/1.e8192fdf.chunk.css"
+  },
+  {
+    "revision": "0209dc54a2db641d1a29a7d6dd6cbbe6",
+    "url": "/index.html"
+  }
+];
